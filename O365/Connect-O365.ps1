4144b7cb34017e9c40bfbd03498124562004b0bc
Lewis Jales-Huggins <lewis.jales@gmail.com>
2017-05-31 19:01:40 +0100

    Add new script to connect to office 365

---
+++ O365/Connect-O365.ps1
@@ -0,0 +1,11 @@
+# Script used to connect to Office 365
+
+# Creates pop up asking to input users login details
+$UserCredential = Get-Credential
+
+# Creates a new session
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+# Imports the created session
+Import-PSSession $Session
+
